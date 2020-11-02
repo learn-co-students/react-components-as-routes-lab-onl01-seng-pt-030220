27017e4de1845cf753ac0cf1e0dc8ee1c6a0bf67
MMcClure11 <mcclme02@luther.edu>
2020-11-02 11:41:40 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,13 +5,16 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
-        {actors.map((actor, i) => 
+        {actors.map((actor, i) => {
+          return(
           <div className="actor" key={i}>
           {actor.name}
           <ul>
             {actor.movies.map((movie, i) => <li key={i}>{movie}</li>)}
           </ul>
           </div>
+          )
+        }
         )}
     </div>
   );
