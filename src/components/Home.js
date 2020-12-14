e2db8563db1da2c79ae0b36e09cff54e945f99e9
kelly_maass <kmaass77627@gmail.com>
2020-12-13 20:00:55 -0600

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      {/*{code here}*/}
+      {}
     </div>
   );
 };
+
+ReactDOM.render((
+<Router>
+
+  <Route path="/" component={Home} />
+</Router>)
+)
 
 export default Home;
